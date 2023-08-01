f88ca1caad3dbca66d32bf5f409fa5e9e41bcc24
kabitaremotework <104657984+kabitaremotework@users.noreply.github.com>
2023-08-01 06:21:20 +0545

    update

--- projects/3-mern-book/backend/controllers/book-controller.js
+++ projects/3-mern-book/backend/controllers/book-controller.js
@@ -30,15 +30,54 @@ const bookById = async (req, res) => {
 const addBook = async (req, res) => {
   const { name, author, desc, price, img, available } = req.body;
 
-  const book = new Book({name, author, price, img, available})
-
+  let book;
   try {
-const book = 
+    book = new Book({ name, author, desc, price, img, available });
+    await book.save();
+  } catch (err) {
+    console.log(err);
   }
-  catch(err) {
-console.log(err)
+  if (!book) {
+    return res.status(500).json({ message: 'Book not added' });
   }
-
+  return res.status(202).json({ book });
 };
 
-module.exports = { allBooks, bookById };
+const updateBook = async (req, res) => {
+  const { name, author, desc, price, img, available } = req.body;
+  const id = req.params.id;
+  let book;
+  try {
+    book = await Book.findByIdAndUpdate(id, {
+      name,
+      author,
+      desc,
+      price,
+      img,
+      available,
+    });
+    await book.save();
+  } catch (err) {
+    console.log(err);
+  }
+  if (!book) {
+    return res.status(500).json({ message: 'Book not updated' });
+  }
+  return res.status(202).json({ book });
+};
+
+const deleteBook =async (req, res)=>{
+  const id = req.params.id;
+  let book;
+  try {
+book = await Book.findByIdAndRemove(id)
+  }catch(err){
+    console.log(err)
+  }
+
+  if (!book) {
+    return res.status(500).json({ message: 'not deleted' });
+  }
+  return res.status(202).json({ message: "Book is deleted" });
+}
+module.exports = { allBooks, bookById, addBook, updateBook, deleteBook };
--- projects/3-mern-book/backend/routes/book-router.js
+++ projects/3-mern-book/backend/routes/book-router.js
@@ -1,10 +1,19 @@
 const express = require('express');
 
-const { allBooks, bookById } = require('../controllers/book-controller');
+const {
+  allBooks,
+  bookById,
+  addBook,
+  updateBook,
+  deleteBook,
+} = require('../controllers/book-controller');
 
 const router = express.Router();
 
 router.get('/', allBooks);
 router.get('/:id', bookById);
+router.post('/', addBook);
+router.put('/:id', updateBook);
+router.delete('/:id', deleteBook);
 
 module.exports = router;
