45607f973b803c841abeeacbd74278e1c8a8830c
369webforall <369webforall@gmail.com>
2023-08-31 15:21:01 +0545

    nextjs

---
+++ nextjs-tutorial/README.md
@@ -0,0 +1,3 @@
+#Nextjs Tutorial - 8/31/2023
+
+[class-1-yt](https://youtu.be/uF9ulchK3jI)
--- projects/5-nextjs-work-manager/src/context/userContext.js
+++ projects/5-nextjs-work-manager/src/context/userContext.js
@@ -1,5 +1,5 @@
-"use client";
-import { createContext } from "react";
+'use client';
+import { createContext } from 'react';
 
 const UserContext = createContext({});
 export default UserContext;
