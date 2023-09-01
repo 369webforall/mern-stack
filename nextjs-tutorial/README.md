0f1f5076cb70995f434c5c9531b7d1434169f774
369webforall <369webforall@gmail.com>
2023-09-01 14:37:18 +0545

    image

--- nextjs-tutorial/README.md
+++ nextjs-tutorial/README.md
@@ -1,3 +1,5 @@
 #Nextjs Tutorial - 8/31/2023
 
 [class-1-yt](https://youtu.be/uF9ulchK3jI)
+
+[class-2-yt](https://youtu.be/1882WzoOh54)
--- nextjs-tutorial/my-app/src/app/page.js
+++ nextjs-tutorial/my-app/src/app/page.js
@@ -1,7 +1,63 @@
 import React from 'react';
-
+import Image from 'next/image';
+import NatureImage from '../../public/images/nature.jpg';
 const Home = () => {
-  return <div className="bg-[#262628] w-full h-screen"></div>;
+  return (
+    <div className="w-full h-screen">
+      <h1>Image from nextjs</h1>
+      <section className="container mx-auto bg-red-600 text-white h-screen grid grid-cols-3 gap-8">
+        <div className="relative bg-slate-500">
+          <Image
+            src={NatureImage}
+            alt="nature"
+            fill
+            priority
+            className="object-cover block"
+          />
+        </div>
+
+        <div className="relative p-5">
+          <Image
+            src={NatureImage}
+            alt="nature"
+            fill
+            priority
+            className="object-contain"
+          />
+        </div>
+
+        <div className="relative p-5">
+          <Image
+            src={NatureImage}
+            alt="nature"
+            fill
+            priority
+            className="object-contain"
+          />
+        </div>
+
+        <div className="relative p-5">
+          <Image
+            src={NatureImage}
+            alt="nature"
+            fill
+            priority
+            className="object-contain"
+          />
+        </div>
+
+        <div className="relative p-5">
+          <Image
+            src="https://images.unsplash.com/photo-1444464666168-49d633b86797?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80"
+            alt="nature"
+            fill
+            priority
+            className="object-contain"
+          />
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
